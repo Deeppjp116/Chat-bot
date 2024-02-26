be0d68f4de2f262d2f08681466a4afb899bdfcb0
deep <deeppjp1656@gmail.com>
2024-02-26 19:03:49 +0530

    Complit Project

--- server/db.js
+++ server/db.js
@@ -1,20 +1,17 @@
-// db.js
 const mongoose = require('mongoose');
 
 let dbURI;
 
 if (process.env.NODE_ENV === 'production') {
-  // Use MongoDB Atlas connection string for production
   dbURI = process.env.MONGODB_URI;
   console.log('Connects to the URI ');
 } else {
-  // Use local MongoDB connection string for development
   dbURI = 'mongodb://localhost:27017/User';
   console.log('Connects to the Locally');
 }
 
 mongoose.connect(dbURI, {
-  serverSelectionTimeoutMS: 5000, // 5 seconds timeout for server selection
+  serverSelectionTimeoutMS: 5000,
   socketTimeoutMS: 45000,
   ssl: true,
 });
